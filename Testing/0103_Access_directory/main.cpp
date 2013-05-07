c76b0c6187a0432bbb8b30ab344a7f3d013b86bd
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2013-05-07 17:15:47 +0800

    新增 Testing/0103_Access_directory

---
+++ Testing/0103_Access_directory/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <dirent.h>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+    std::string query;
+
+		/* parse arguements */{
+			if(argc == 1){
+				query = ".";
+			}else if(argc == 2){
+				query = argv[1];
+			}else{
+				std::cerr << "命令格式輸入錯誤！" << std::endl;
+			}
+    }
+
+		/* access directory, then output all items' name */{
+			DIR *dir_stream;
+			struct dirent *dir_entry;
+
+			dir_stream = opendir(query.c_str());
+			if(dir_stream == NULL){
+				std::cerr << "目錄開啟失敗！" << std::endl;
+			}else{
+				while((dir_entry = readdir(dir_stream)) != NULL){
+					std::cout << dir_entry->d_name << std::endl;
+				}
+				closedir(dir_stream);
+			}
+		}
+    return 0;
+}
